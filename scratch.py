14b13de5de61f500430a487c0d932468ff608dde
Ryan Danehy <rdanehy@iu.edu>
2021-03-07 14:33:02 -0500

    Example python file

---
+++ scratch.py
@@ -0,0 +1,35 @@
+def function():
+    print("Hello")
+    # 10
+
+
+if __name__ == '__main__':
+#     print("Hello World")
+#     print(Hello World)
+# #
+#     x = 5
+#     variable = 6
+#
+#     if x == variable:
+#         print("if", x, variable)
+#     else:
+#         print("else", x, variable)
+#
+#     x = "hello"
+#     variable = "hello"
+#
+#     if x == variable:
+#         print("if", x, variable)
+#     else:
+#         print("else", x, variable)
+# #
+#
+    array = [5,1,2,3,4,5,6,7,8,9]
+    print(array, array[0])
+
+    for i in range(len(array)):
+        print('iteration= ', i+1, 'i =', i)
+    print(array)
+#     function()
+#     variable = function()
+#     print(variable)
